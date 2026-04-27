036541ba3ef84f74d3a63cd7e4895d6479cd57f7
jlassi mazen <jlmazen1073@gmail.com>
2026-04-27 01:55:03 +0100

    scheduling the analitics collector

---
+++ src/main/java/com/example/metatry/Services/AnalyticsScheduler.java
@@ -0,0 +1,19 @@
+package com.example.metatry.Services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AnalyticsScheduler {
+
+    private final AnalyticsService analyticsService;
+
+
+    @Scheduled(fixedRate = 6 * 60 * 60 * 1000)
+    public void collectAnalytics() {
+        System.out.println("Running scheduled analytics...");
+        analyticsService.collectMetricsForPublishedPosts();
+    }
+}
